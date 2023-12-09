c5c5ce7fee2ff91ba34b36257b6ab0e827c61d83
Iurii Iaremenko <iurii.iaremenko@gmail.com>
2023-12-09 12:00:48 +0200

    Initial Commit

---
+++ CraftTestTask/CraftTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraftTestTaskApp.swift
+//  CraftTestTask
+//
+//  Created by iurii iaremenko on 09.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CraftTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
